d5cefd12c7ee13f0370a81818e7b6d82c0c84987
Mathieu Lecarme <mlecarme@bearstech.com>
2015-09-23 23:56:39 +0200

    Application factory, count batch, chrono.

--- tsuna.py
+++ tsuna.py
@@ -1,31 +1,58 @@
+import json
 import asyncio
 
 import asyncio_redis
 import aiohttp
 
 
+class Chrono:
+
+    def __init__(self, redis, loop, args):
+        self.redis = redis
+        self.loop = loop
+        self.args = args
+
+    def __enter__(self):
+        self.clock = self.loop.time()
+
+    def __exit__(self, *args):
+        self.loop.create_task(
+            self.redis.lpush('stats', [
+                "%s|%f" % ("|".join(self.args),
+                           (self.loop.time() - self.clock))])
+        )
+
+
 class Application(dict):
-    n = 0
+
+    def __init__(self, loop):
+        self.loop = loop
 
     def finish(self):
         pass
+
+    def chrono(self, *args):
+        return Chrono(self['redis'], self.loop, args)
 
     @asyncio.coroutine
     def forever(self):
         redis = self['redis']
         ping = yield from redis.ping()
-        print("ping", ping)
+        assert ping.status == "PONG"
         subscriber = yield from redis.start_subscribe()
         chan = yield from subscriber.subscribe(['/tsunami'])
-        loop = asyncio.get_event_loop()
-        print(subscriber)
         print("SUBSCRIBE")
+        def end_scenario(f):
+            print("END OF TSUNAMI")
         while True:
             reply = yield from subscriber.next_published()
+            print("TSUNAMI!", reply)
+            batch = []
             for d in reply.value.split():
                 for a in range(10):
-                    t = loop.create_task(self.handle(d))
-            print(reply)
+                    batch.append(self.loop.create_task(self.handle(d)))
+            self.loop.create_task(asyncio.wait(batch))\
+                .add_done_callback(end_scenario)
 
     @asyncio.coroutine
     def handle(self, arg):
@@ -33,8 +60,8 @@ class Application(dict):
 
 
 @asyncio.coroutine
-def init(loop):
-    app = Application()
+def application_factory(application=Application, loop=asyncio.get_event_loop()):
+    app = application(loop)
     app['redis'] = yield from asyncio_redis.Pool.create(host='127.0.0.1',
                                                         port=6379, poolsize=10)
     return app
