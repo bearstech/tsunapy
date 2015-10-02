ea15894dae41752f09c4193b17614107f96c062f
Mathieu Lecarme <mlecarme@bearstech.com>
2015-10-02 19:10:49 +0200

    Lets the nzumbe working.

--- tsuna.py
+++ tsuna.py
@@ -3,6 +3,11 @@ import asyncio
 
 import asyncio_redis
 import aiohttp
+from aiohttp.client import ClientSession
+
+from uuid import uuid4
+
+from nzumbe import Nzumbe
 
 
 class Chrono:
@@ -22,22 +27,30 @@ class Chrono:
                            (self.loop.time() - self.clock))])
         )
 
+
 class Session:
 
     def __init__(self, session, redis, loop):
         self.session = session
         self.redis = redis
         self.loop = loop
+        self.uuid = uuid4().hex
+        self._clock = self.loop.time()
 
     def chrono(self, *args):
         return Chrono(self.redis, self.loop, args)
 
     @asyncio.coroutine
-    def get(self, tags, url, *args, **kargs):
-        with self.chrono(*tags):
+    def get(self, tag, url, *args, **kargs):
+        with self.chrono(self.uuid, tag):
             resp = yield from self.session.get(url, *args, **kargs)
             body = yield from resp.read()
         return resp, body
+
+    def close(self):
+        self.loop.create_task(self.redis.lpush(
+                'stats', ["%s|scenario|%f"  % (self.uuid, self.loop.time() - self._clock)]))
+        self.session.close()
 
 
 class Application(dict):
@@ -45,38 +58,11 @@ class Application(dict):
     def __init__(self, loop):
         self.loop = loop
 
-    def finish(self):
-        pass
-
     def chrono(self, *args):
         return Chrono(self['redis'], self.loop, args)
 
     def session(self):
         return Session(aiohttp.ClientSession(loop=self.loop), self['redis'], self.loop)
-
-    @asyncio.coroutine
-    def forever(self):
-        redis = self['redis']
-        ping = yield from redis.ping()
-        assert ping.status == "PONG"
-        subscriber = yield from redis.start_subscribe()
-        chan = yield from subscriber.subscribe(['/tsunami'])
-        print("SUBSCRIBE")
-        def end_scenario(f):
-            print("END OF TSUNAMI")
-        while True:
-            reply = yield from subscriber.next_published()
-            print("TSUNAMI!", reply)
-            batch = []
-            for d in reply.value.split():
-                for a in range(10):
-                    batch.append(self.loop.create_task(self.handle(d)))
-            self.loop.create_task(asyncio.wait(batch))\
-                .add_done_callback(end_scenario)
-
-    @asyncio.coroutine
-    def handle(self, arg):
-        raise NotImplemented()
 
 
 @asyncio.coroutine
@@ -85,3 +71,12 @@ def application_factory(application=Application, loop=asyncio.get_event_loop()):
     app['redis'] = yield from asyncio_redis.Pool.create(host='127.0.0.1',
                                                         port=6379, poolsize=10)
     return app
+
+
+class App:
+
+    def __init__(self, loop=asyncio.get_event_loop()):
+        self.loop = loop
+
+    def task(self):
+        pass
